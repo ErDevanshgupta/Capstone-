2b6265ee7060c6c51f63b97b567035a10783d2eb
Devansh Gupta <110588013+ErDevanshgupta@users.noreply.github.com>
2024-10-07 10:24:55 +0530

    Update control.h

--- control.h
+++ control.h
@@ -6,24 +6,21 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <ns3/net-device.h>
 
-// Define Node structure
 struct Node {
     int id;
     double energy;
-    double x, y, z; // Coordinates
     bool isMalicious;
     double trustScore;
 };
 
-// Define Packet structure
 struct Packet {
     int src;
     int dest;
     std::vector<int> path;
 };
 
-// SDN Controller Class
 class SDNController {
 public:
     SDNController();
@@ -33,18 +30,21 @@ public:
     void evaluateTrust();
     void updateRouting();
     void handlePacket(Packet &pkt);
-    void logToPCAP(const std::string &filename, const Packet &pkt);
+    void logParameters(const std::string &filename);
+
+    // New function to enable PCAP tracing
+    void EnablePcapTracing();
 
 private:
     std::vector<Node> nodes;
-    std::map<int, std::vector<int>> routingTable; // Node ID -> List of neighbor IDs
+    std::map<int, std::vector<int>> routingTable;
+    std::vector<ns3::Ptr<ns3::NetDevice>> devices;  // List of network devices
 
     double computeCommunicationTrust(const Node &node);
     double computeNodeTrust(const Node &node);
     double computeEnvironmentTrust(const Node &node);
     std::vector<int> getOptimalPath(int src, int dest);
     double runLSTMModel(const std::vector<double> &trustMetrics);
-    double computeReward(int src, int dest, const std::vector<int> &path);
 };
 
-#endif // CONTROL_H
+#endif  // CONTROL_H
