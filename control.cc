f6063c0577a51499489dc5575a1f5d2b7b2d71b0
Devansh Gupta <110588013+ErDevanshgupta@users.noreply.github.com>
2024-10-08 15:42:39 +0530

    Update control.cc

--- control.cc
+++ control.cc
@@ -1,7 +1,10 @@
 #include "control.h"
 #include "ns3/net-device-container.h"
-#include "ns3/ptr.h"
+#include "ns3/point-to-point-helper.h"
+#include "ns3/ipv4-address-helper.h"
+#include "ns3/internet-stack-helper.h"
 #include "ns3/log.h"
+#include "ns3/pcap-file-wrapper.h"
 
 using namespace ns3;
 using namespace std;
@@ -11,21 +14,39 @@ NS_LOG_COMPONENT_DEFINE("SDNController");
 NetDeviceContainer SDNController::initializeNetwork(int numNodes) {
     NodeContainer nodes;
     nodes.Create(numNodes);
-    
-    // Add code to configure devices, channels, etc.
+
+    // Install internet stack
+    InternetStackHelper internet;
+    internet.Install(nodes);
+
+    // Create a point-to-point link between each pair of nodes
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
     NetDeviceContainer netDevices;
-    
-    // Example device configuration:
-    // Add your network initialization code here
+
+    // Connect nodes in pairs, assuming even number of nodes
+    for (int i = 0; i < numNodes - 1; i++) {
+        NetDeviceContainer linkDevices = pointToPoint.Install(nodes.Get(i), nodes.Get(i + 1));
+        netDevices.Add(linkDevices);
+    }
+
+    // Assign IP addresses
+    Ipv4AddressHelper ipv4;
+    ipv4.SetBase("10.1.1.0", "255.255.255.0");
+
+    for (size_t i = 0; i < netDevices.GetN(); i += 2) {
+        ipv4.Assign(netDevices.Get(i));
+    }
 
     return netDevices;
 }
 
 void SDNController::EnablePcapTracing(NetDeviceContainer devices) {
     for (size_t i = 0; i < devices.GetN(); i++) {
-        // Enable PCAP tracing for each device
-        PcapHelper pcapHelper;
-        pcapHelper.EnablePcap("trace", devices.Get(i), true);
+        Ptr<NetDevice> device = devices.Get(i);
+        device->EnablePcap("trace", device, true);
     }
 }
 
