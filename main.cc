9d5719ef6e85154375771170eb0c9c680d2f6656
Devansh Gupta <110588013+ErDevanshgupta@users.noreply.github.com>
2024-10-08 15:33:16 +0530

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,10 +1,9 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/pcap-file-wrapper.h"
 #include "control.h"
-#include <ns3/core-module.h>
-#include <ns3/csma-module.h>
-#include <ns3/internet-module.h>
-#include <ns3/network-module.h>
-#include <ns3/ofswitch13-module.h>
-#include <ns3/aqua-sim-helper.h>
 
 using namespace ns3;
 
@@ -15,18 +14,13 @@ int main(int argc, char *argv[]) {
     nodes.Create(3); // Example: Create 3 nodes
 
     // Initialize the network
-    controller.initializeNetwork(nodes.GetN());
+    NetDeviceContainer netDevices = controller.initializeNetwork(nodes.GetN());
 
-    // Obtain the NetDeviceContainer (assuming you have it from controller.initializeNetwork or another source)
-    NetDeviceContainer netDevices; // This should be the container of your network devices
-    // Typically, netDevices would be filled with the devices during initialization
-
-    // Pass the NetDeviceContainer to EnablePcapTracing
-    controller.EnablePcapTracing(netDevices);
+    // Enable PCAP tracing (Pass netDevices)
+    controller.EnablePcapTracing(netDevices); 
 
     // Run the simulation
     Simulator::Run();
     Simulator::Destroy();
     return 0;
 }
-
