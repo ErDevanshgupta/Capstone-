b3324e2debfddfe7e0ad0a0526320c6acd20a0eb
Devansh Gupta <110588013+ErDevanshgupta@users.noreply.github.com>
2024-10-09 07:04:23 +0530

    Update main.cc

--- main.cc
+++ main.cc
@@ -5,12 +5,8 @@
 #include <ns3/network-module.h>
 #include <ns3/ofswitch13-module.h>
 #include <ns3/aqua-sim-helper.h>
+#include <ns3/v4ping-helper.h>  // Include Ping module
 #include <ns3/ipv4-static-routing-helper.h>
-#include <ns3/ipv4-routing-helper.h>
-#include <ns3/ipv4-static-routing.h>
-#include <ns3/ipv4-routing-table-entry.h>
-#include <ns3/applications-module.h>
-#include <ns3/v4ping-helper.h> // Include the v4ping module for pinging
 
 using namespace ns3;
 
@@ -31,24 +27,24 @@ int main(int argc, char *argv[])
     SDNController controller;
     int numNodes = 50; // Number of nodes in the network
 
-    // Initialize network in SDN controller and get the devices
+    // Initialize network in SDN controller and get devices
     NetDeviceContainer netDevices = controller.initializeNetwork(numNodes);
 
     // Setup ICMP ping between nodes
     uint32_t sourceNodeIndex = 0;
     uint32_t destNodeIndex = numNodes - 1;
 
-    Ptr<Node> sourceNode = NodeList::GetNode(sourceNodeIndex); // Correct use of Ptr<Node>
+    Ptr<Node> sourceNode = NodeList::GetNode(sourceNodeIndex);
     Ptr<Node> destNode = NodeList::GetNode(destNodeIndex);
 
-    Ipv4Address destAddress = destNode->GetObject<Ipv4>()->GetAddress(1, 0).GetLocal(); // Get the IP of the destination node
+    Ipv4Address destAddress = destNode->GetObject<Ipv4>()->GetAddress(1, 0).GetLocal(); // Get destination IP
 
     // Install Ping Application on the source node
-    V4PingHelper ping(destAddress); // Correct usage of V4PingHelper
+    V4PingHelper ping(destAddress); // Use V4PingHelper for the ping application
     ping.SetAttribute("Verbose", BooleanValue(true));
     ApplicationContainer app = ping.Install(sourceNode);
     app.Start(Seconds(1.0));  // Start pinging at time = 1s
-    app.Stop(Seconds(simTime - 1));
+    app.Stop(Seconds(simTime - 1)); // Stop before the end of simulation
 
     if (verbose) {
         LogComponentEnable("OFSwitch13Device", LOG_LEVEL_ALL);
