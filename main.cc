ee2ab3fa0e48380342582714c3dabff130f193d4
Devansh Gupta <110588013+ErDevanshgupta@users.noreply.github.com>
2024-10-09 06:11:34 +0530

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,28 +1,51 @@
-#include "ns3/core-module.h"
-#include "ns3/network-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/pcap-file-wrapper.h"
 #include "control.h"
+#include <ns3/core-module.h>
+#include <ns3/csma-module.h>
+#include <ns3/internet-module.h>
+#include <ns3/network-module.h>
+#include <ns3/ofswitch13-module.h>
+#include <ns3/aqua-sim-helper.h>
 
 using namespace ns3;
-int main(int argc, char *argv[]) {
-    // Set up the network and nodes
+
+int main(int argc, char *argv[])
+{
+    uint16_t simTime = 10; // Simulation time in seconds
+    bool verbose = false;
+    bool trace = true; // Enable PCAP traces
+
+    // Configure command line parameters
+    CommandLine cmd;
+    cmd.AddValue("simTime", "Simulation time (seconds)", simTime);
+    cmd.AddValue("verbose", "Enable verbose output", verbose);
+    cmd.AddValue("trace", "Enable PCAP tracing", trace);
+    cmd.Parse(argc, argv);
+
+    // Initialize the SDN controller
     SDNController controller;
-    NodeContainer nodes;
-    nodes.Create(3); // Example: Create 3 nodes
+    int numNodes = 50; // Number of nodes in the network
 
-    // Initialize the network and get the NetDeviceContainer
-    NetDeviceContainer netDevices = controller.initializeNetwork(nodes.GetN());
+    // Initialize network in SDN controller
+    controller.initializeNetwork(numNodes);
 
-    // Enable PCAP tracing (Pass netDevices)
-    controller.EnablePcapTracing(netDevices); 
+    if (verbose) {
+        LogComponentEnable("OFSwitch13Device", LOG_LEVEL_ALL);
+        LogComponentEnable("OFSwitch13Controller", LOG_LEVEL_ALL);
+        LogComponentEnable("SDNController", LOG_LEVEL_ALL);
+    }
+
+    // Evaluate trust for nodes
+    controller.evaluateTrust();
+
+    // Enable tracing (PCAP format)
+    if (trace) {
+        controller.EnablePcapTracing();
+    }
 
     // Run the simulation
+    Simulator::Stop(Seconds(simTime));
     Simulator::Run();
     Simulator::Destroy();
+
     return 0;
 }
-
-
-
