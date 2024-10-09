810ca0444778b17649ace2e2ca2ee24fe6c6cd8d
Devansh Gupta <110588013+ErDevanshgupta@users.noreply.github.com>
2024-10-09 06:12:25 +0530

    Update control.cc

--- control.cc
+++ control.cc
@@ -1,96 +1,106 @@
 #include "control.h"
-#include "ns3/net-device-container.h"
-#include "ns3/point-to-point-helper.h"
-#include "ns3/ipv4-address-helper.h"
-#include "ns3/internet-stack-helper.h"
-#include "ns3/log.h"
-#include "ns3/pcap-file-wrapper.h"
-#include "ns3/point-to-point-helper.h"
+#include <ns3/csma-helper.h>
+#include <ns3/internet-module.h>
+#include <ns3/net-device.h>
+#include <ns3/ipv4-address-helper.h>
+#include <iostream>
+#include <cmath> // For trust calculations
 
-using namespace ns3;
-using namespace std;
+SDNController::SDNController() {
+    // Constructor implementation
+}
 
-NS_LOG_COMPONENT_DEFINE("SDNController");
-
-class SDNController {
-public:
-    void initializeNetwork(int numNodes);
-    void EnablePcapTracing(NetDeviceContainer devices);
-};
+SDNController::~SDNController() {
+    // Destructor implementation
+}
 
 void SDNController::initializeNetwork(int numNodes) {
-    NodeContainer nodes;
+    ns3::NodeContainer nodes;
     nodes.Create(numNodes);
 
-    // Create a PointToPointHelper for configuring point-to-point links
-    PointToPointHelper pointToPoint;
-    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+    // Use CsmaHelper for wired connections (or AquaSimHelper for UASNs)
+    ns3::CsmaHelper csmaHelper;
+    csmaHelper.SetChannelAttribute("DataRate", ns3::DataRateValue(ns3::DataRate("100Mbps")));
+    csmaHelper.SetChannelAttribute("Delay", ns3::TimeValue(ns3::MilliSeconds(2)));
 
-    // Connect the nodes
-    for (uint32_t i = 0; i < numNodes - 1; ++i) {
-        NetDeviceContainer devices = pointToPoint.Install(nodes.Get(i), nodes.Get(i + 1));
-        // You can store devices for PCAP tracing if necessary
+    // Install devices on all nodes
+    ns3::NetDeviceContainer netDevices = csmaHelper.Install(nodes);
+
+    // Add devices to the vector for PCAP tracing
+    for (uint32_t i = 0; i < netDevices.GetN(); ++i) {
+        devices.push_back(netDevices.Get(i));
+    }
+
+    // Install Internet stack (IP, routing, etc.) on the nodes
+    ns3::InternetStackHelper internet;
+    internet.Install(nodes);
+
+    // Assign IP addresses to devices
+    ns3::Ipv4AddressHelper ipv4;
+    ipv4.SetBase("10.1.1.0", "255.255.255.0");
+    ipv4.Assign(netDevices);
+
+    // Initialize nodes for trust evaluation
+    for (int i = 0; i < numNodes; ++i) {
+        Node node;
+        node.id = i;
+        node.energy = 100.0; // Initial energy value (could be dynamic)
+        node.trustScore = 0.5; // Initialize trust score to neutral value
+        node.isMalicious = false;
+        nodes.push_back(node);
     }
 }
 
-void SDNController::EnablePcapTracing(NetDeviceContainer devices) {
-    for (size_t i = 0; i < devices.GetN(); ++i) {
-        Ptr<NetDevice> device = devices.Get(i);
-        // Use the PcapFileWrapper to enable tracing
-        PcapFileWrapper file;
-        std::ostringstream oss;
-        oss << "trace_" << i << ".pcap";
-        file.Open(oss.str(), std::ios::out | std::ios::binary);
-        device->TraceConnectWithoutContext("Tx", MakeCallback(&PcapFileWrapper::Trace, &file));
+void SDNController::EnablePcapTracing() {
+    ns3::CsmaHelper csmaHelper;
+
+    if (devices.empty()) {
+        std::cerr << "No devices initialized for PCAP tracing!" << std::endl;
+        return;
+    }
+
+    for (size_t i = 0; i < devices.size(); ++i) {
+        std::string filename = "/home/capstone/trace/devansh/trace/node" + std::to_string(i) + ".pcap";
+        csmaHelper.EnablePcap(filename, devices[i], true);
+        std::cout << "PCAP logging enabled for node " << i << " -> " << filename << std::endl;
     }
 }
 
-
-
-void SDNController::calculateNodeTrust(NodeContainer& nodes) {
-    for (NodeContainer::Iterator i = nodes.Begin(); i != nodes.End(); ++i) {
-        Ptr<Node> node = *i;
-        double trustValue = computeNodeTrust(node);
-        NS_LOG_INFO("Node " << node->GetId() << " Trust: " << trustValue);
+void SDNController::evaluateTrust() {
+    for (Node &node : nodes) {
+        node.trustScore = computeNodeTrust(node);
+        std::cout << "Node " << node.id << " trust score: " << node.trustScore << std::endl;
     }
 }
 
-double SDNController::computeNodeTrust(Ptr<Node> node) {
-    // Placeholder logic for computing node trust
-    double trustValue = 1.0; // Example value
-    return trustValue;
+double SDNController::computeNodeTrust(const Node &node) {
+    // Weighting factors
+    double alpha = 0.4, beta = 0.3, gamma = 0.3;
+
+    double T_comm = computeCommunicationTrust(node); // Communication trust
+    double T_behave = computeBehaviorTrust(node); // Node behavior trust
+    double T_env = computeEnvironmentTrust(node); // Environmental trust
+
+    // Total trust score as weighted sum of all factors
+    return alpha * T_comm + beta * T_behave + gamma * T_env;
 }
 
-void SDNController::lstmTrafficPrediction() {
-    vector<double> trafficData = loadTrafficData();
-    vector<double> predictions = runLstmModel(trafficData);
+double SDNController::computeCommunicationTrust(const Node &node) {
+    // Example: Based on packet delivery success (simulated as a random value)
+    double deliverySuccessRate = 0.9; // Example fixed value, this should come from the network stats
+    return deliverySuccessRate; // Return value between 0 and 1
+}
 
-    // Log predictions
-    for (size_t i = 0; i < predictions.size(); ++i) {
-        NS_LOG_INFO("Prediction " << i << ": " << predictions[i]);
+double SDNController::computeBehaviorTrust(const Node &node) {
+    // Example: Based on packet drops or malicious activity (simulated here)
+    if (node.isMalicious) {
+        return 0.1; // Low trust for malicious nodes
     }
+    return 0.8; // Example for well-behaved nodes
 }
 
-vector<double> SDNController::loadTrafficData() {
-    vector<double> data;
-    // Load traffic data here (example values)
-    data.push_back(1.0);
-    data.push_back(2.5);
-    data.push_back(3.7);
-    return data;
+double SDNController::computeEnvironmentTrust(const Node &node) {
+    // Example: Based on node energy
+    double maxEnergy = 100.0;
+    return node.energy / maxEnergy; // Return value between 0 and 1
 }
-
-vector<double> SDNController::runLstmModel(const vector<double>& inputData) {
-    vector<double> outputData;
-    // Example LSTM logic (placeholder)
-    for (double value : inputData) {
-        outputData.push_back(value * 1.1); // Example transformation
-    }
-    return outputData;
-}
-
-
-
-
-
